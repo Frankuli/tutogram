873d2ac470186c3f63db8644c5025f2af38f88cc
Franco <franco.mana@hotmail.com>
2018-06-21 15:28:12 -0300

    error subir imagen

--- src/App.js
+++ src/App.js
@@ -9,30 +9,71 @@ class App extends Component {
   constructor () {
       super();
       this.state = {
-        user: null
+        user: null,
+        pictures: []
       };
+
       this.handleAuth = this.handleAuth.bind(this);
       this.handleLogout = this.handleLogout.bind(this);
+      this.handleUpload = this.handleUpload.bind(this);
   }
 
   componentWillMount () {
     firebase.auth().onAuthStateChanged( user => {
       this.setState({ user });
     });
+
+    firebase.database().ref('pictures').on('child_added', snapshot => {
+      this.setState({
+        pictures: this.state.pictures.concat(snapshot.val())
+      });
+    });
   }
 
   handleAuth () {
-    const provider = new firebase.auth.GoogleAuthProvider(); //provedor para Google
+    const provider = new firebase.auth.GoogleAuthProvider();
+
     firebase.auth().signInWithPopup(provider)
-      .them(result => console.log(`${result.user.email} ha inisiado sesion`))
-      .catch(error => console.log(`Error ${error.code}: ${error.massage}`));
+      .then(result => console.log(`${result.user.email} ha iniciado sesión`))
+      .catch(error => console.log(`Error ${error.code}: ${error.message}`));
   }
 
   handleLogout () {
     firebase.auth().signOut()
-      .them(result => console.log(`${result.user.email} ha salido`))
-      .catch(error => console.log(`Error ${error.code}: ${error.massage}`));
+      .then(result => console.log(`${result.user.email} ha iniciado sesión`))
+      .catch(error => console.log(`Error ${error.code}: ${error.message}`));
   }
+
+  handleUpload (event) {
+    const file =  event.target.files[0];
+    const storageRef = firebase.storage().ref('/fotos/');
+    var task = storageRef.child(`${file.name}`).put(file);
+
+    task.on('state_changed', (snapshot) => {
+      let percentage = (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+      this.setState({
+        uploadValue: percentage
+      })
+    }, (error) => {
+      console.error(error.message)
+    }, () => {/*
+      console.log(task.snapshot);
+      storageRef.child(file.name).getDownloadURL().then((url) => {
+        this.setState({
+          picture: url
+        })
+      })*/
+      const record = {
+        photoURL: this.state.user.photoURL,
+        displayName: this.state.user.displayName,
+        image: task.snapshot.downloadURL
+      }
+      const dbRef = firebase.database().ref('pictures');
+      const newPicture = dbRef.push();
+      newPicture.set(record);
+    });
+
+  }﻿
 
   renderLoginButton () {
     // si el user esta logeado
@@ -42,7 +83,19 @@ class App extends Component {
           <img width="100px" height="100px" src= { this.state.user.photoURL}/>
           <p> Hola {this.state.user.displayName}! </p>
           <button onClick= { this.handleLogout }>Salir</button>
-          <FileUpload/>
+
+
+          <FileUpload onUpload={ this.handleUpload }/>
+
+          {
+            this.state.pictures.map(picture => {
+              <div >
+                <img src={ picture.image } /><br/>
+                <img src={ picture.photoURL }/><br/>
+              <span>{ picture.displayName}</span>
+              </div>
+            })
+          }
         </div>
       );
     }else {
